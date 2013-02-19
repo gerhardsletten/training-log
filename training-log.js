6427720e4e9ca67c6327d288b3034fff36da42ae
Gerhard Sletten <gerhardsletten@gmail.com>
2013-02-19 11:08:52 +0100

    removed unused functions in js-api

--- training-log.js
+++ training-log.js
@@ -15,41 +15,10 @@
 			}
 		});
 	},
-	TrainingLog.editSession = function(obj, callback, error_callback) {
-		$.post(TrainingLog.ajaxurl, {
-			action : 'editSession',
-			data : obj,
-			nonce : TrainingLog.nonce
-		} ,function(data){
-			if(data.error && error_callback) {
-				error_callback(data.error);
-				return;
-			}
-			if(callback) {
-				callback(data);
-			}
-		});
-	},
 	TrainingLog.deleteSession = function(id, callback, error_callback) {
 		$.post(TrainingLog.ajaxurl, {
 			action : 'deleteSession',
 			id : id,
-			nonce : TrainingLog.nonce
-		} ,function(data){
-			if(data.error && error_callback) {
-				error_callback(data.error);
-				return;
-			}
-			if(callback) {
-				callback(data);
-			}
-		});
-	},
-	TrainingLog.listSessions = function(page, limit, callback, error_callback) {
-		$.post(TrainingLog.ajaxurl, {
-			action : 'listSessions',
-			page : page,
-			limit: limit,
 			nonce : TrainingLog.nonce
 		} ,function(data){
 			if(data.error && error_callback) {
